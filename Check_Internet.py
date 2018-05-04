15c082d62e079458cfa2b4c3fc1d261752457d52
Niklas Roth <niklasroth@Niklass-MBP.local>
2018-05-04 19:36:08 +0200

    functionized similar parts. Arbitrary number of network addresses can now be defined and checked

--- Check_Internet.py
+++ Check_Internet.py
@@ -3,35 +3,58 @@ import time
 import datetime
 import csv
 
-print("========")
 print("========================")
-print("======================================================================")
-print("========================")
-print("========\n")
 print("Start Connection Test \n")
 print("========================")
-print("========================")
-print("========================")
+
 
 ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
-msg1 = "WAN is running"
-msg2 = "WAN is down"
-msg3 = "LAN is running"
-msg4 = "LAN is down"
-NW_ID_1 = "google.com"
-NW_ID_2 = "192.168.1.1"
+msg_success = "is running"
+msg_fail = "is down"
+inverval_seconds = 10  
+
+network_addresses = {"WAN":"www.google.com", "LAN":"192.168.1.1"}
+
+#print("***********************************************************************")
+#print("The following network addresses will be checked")
+#for i in network_addresses:
+#    print("{}: {}".format(i, network_addresses[i]))
+#print("***********************************************************************")
 
 run_time_in_hours = 24
 run_time_in_seconds = run_time_in_hours * 3600
 start_time = int (ts)
 end_time = start_time + run_time_in_seconds
 
-print('start time is: {}'.format(start_time))
-print('end time is: {}'.format(end_time))
+#print('start time is: {}'.format(start_time))
+#print('end time is: {}'.format(end_time))
 
 status = ""
 counter = 0
+
+title = "Networkprotokol"
+company = " GGA-Maur"
+star_line = "****************************************"
+
+with open('eggs.csv', 'a') as csvfile:
+    active_document = csv.writer(csvfile)
+    active_document.writerow([star_line])
+    active_document.writerow([title]+[company])
+    active_document.writerow([star_line])
+    active_document.writerow([])
+
+
+    for i in network_addresses:
+        string = str(i)+ ": " + str(network_addresses[i])
+        active_document.writerow([string])
+        
+    
+    active_document.writerow([])
+        
+    
+            
 while(int(time.time()) <= end_time):
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
@@ -41,41 +64,22 @@ while(int(time.time()) <= end_time):
     print("<<<----------------------->>>\n")
     counter+=1
     
-    status = check_ping.check_ping(NW_ID_1)
-    if status == "Network Active":
-
+    
+    for i in network_addresses:
+    
         with open('eggs.csv', 'a') as csvfile:
-            spamwriter = csv.writer(csvfile)
-            spamwriter.writerow([counter]+[st]+[NW_ID_1]+[msg1])
+            active_document = csv.writer(csvfile)
+            
+            status = check_ping.check_ping(network_addresses[i])
+            if status == "Network Active":
+                active_document.writerow([counter]+[st]+[i]+[msg_success])
 
-        print(msg1)   
+            # print(msg_success)   
 
-    else:
-        with open('eggs.csv', 'a') as csvfile:
-            spamwriter = csv.writer(csvfile)
-            spamwriter.writerow([counter]+[st]+[NW_ID_1]+[msg2])
+            else:
+                active_document.writerow([counter]+[st]+[i]+[msg_fail])
 
-        print(msg2)
-
-
-    status = check_ping.check_ping(NW_ID_2)
-    if status == "Network Active":
-
-        with open('eggs.csv', 'a') as csvfile:
-            spamwriter = csv.writer(csvfile)
-            spamwriter.writerow([counter]+[st]+[NW_ID_2]+[msg3])
-
-        print(msg3)    
-
-    else:
-        with open('eggs.csv', 'a') as csvfile:
-            spamwriter = csv.writer(csvfile)
-            spamwriter.writerow([counter]+[st]+[NW_ID_2]+[msg4])
-
-        print(msg4)
+            # print(msg_fail)
         
     
-    time.sleep(10)
-        
-
-print("real status" + status)
+    time.sleep(inverval_seconds)
