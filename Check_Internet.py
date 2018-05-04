737ff4dc8fd89baa8e6722f6b28a74efe8fd0924
radioman85 <nikicello@gmail.com>
2018-05-04 14:30:02 +0000

    Finding-Repair: Timestamp was generated outside of loop! No the timestamp is taken inside the loop.

--- Check_Internet.py
+++ Check_Internet.py
@@ -14,15 +14,13 @@ print("========================")
 print("========================")
 
 ts = time.time()
-st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-print(st)
 
 msg1 = "WAN is running"
 msg2 = "WAN is down"
 msg3 = "LAN is running"
 msg4 = "LAN is down"
-ping1 = "google.com"
-ping2 = "192.168.1.1"
+NW_ID_1 = "google.com"
+NW_ID_2 = "192.168.1.1"
 
 run_time_in_hours = 24
 run_time_in_seconds = run_time_in_hours * 3600
@@ -35,42 +33,44 @@ print('end time is: {}'.format(end_time))
 status = ""
 counter = 0
 while(int(time.time()) <= end_time):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
    
     print("\n<<<----------------------->>>")
     print("             {}".format(counter))
     print("<<<----------------------->>>\n")
     counter+=1
     
-    status = check_ping.check_ping(ping1)
+    status = check_ping.check_ping(NW_ID_1)
     if status == "Network Active":
 
         with open('eggs.csv', 'a') as csvfile:
             spamwriter = csv.writer(csvfile)
-            spamwriter.writerow([st]+[msg1])
+            spamwriter.writerow([counter]+[st]+[NW_ID_1]+[msg1])
 
         print(msg1)   
 
     else:
         with open('eggs.csv', 'a') as csvfile:
             spamwriter = csv.writer(csvfile)
-            spamwriter.writerow([st]+[msg2])
+            spamwriter.writerow([counter]+[st]+[NW_ID_1]+[msg2])
 
         print(msg2)
 
 
-    status = check_ping.check_ping(ping2)
+    status = check_ping.check_ping(NW_ID_2)
     if status == "Network Active":
 
         with open('eggs.csv', 'a') as csvfile:
             spamwriter = csv.writer(csvfile)
-            spamwriter.writerow([st]+[msg3])
+            spamwriter.writerow([counter]+[st]+[NW_ID_2]+[msg3])
 
         print(msg3)    
 
     else:
         with open('eggs.csv', 'a') as csvfile:
             spamwriter = csv.writer(csvfile)
-            spamwriter.writerow([st]+[msg4])
+            spamwriter.writerow([counter]+[st]+[NW_ID_2]+[msg4])
 
         print(msg4)
         
